c217a819f794703671be231a3dad4ef529a050ab
Geetansh2002 <geetanshdwivedi1@gmail.com>
2022-11-17 15:57:41 +0530

    activity connected

--- app/src/main/java/com/example/trackhub/MainActivity.kt
+++ app/src/main/java/com/example/trackhub/MainActivity.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
  class MainActivity : AppCompatActivity() {
+     private lateinit var phone:EditText
+     private lateinit var password: EditText
+     private lateinit var b1:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        phone=findViewById(R.id.editTextPhone2)
+        password=findViewById(R.id.editTextTextPassword)
+        b1=findViewById(R.id.button)
+        b1.setOnClickListener{
+            val intent=Intent(this,home::class.java)
+            startActivity(intent)
+            }
     }
  }
