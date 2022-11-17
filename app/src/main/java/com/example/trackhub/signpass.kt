76736792633b7003fe9c9244e942b6c7e40997af
Geetansh2002 <geetanshdwivedi1@gmail.com>
2022-11-17 21:06:52 +0530

    ui updates

--- app/src/main/java/com/example/trackhub/signpass.kt
+++ app/src/main/java/com/example/trackhub/signpass.kt
@@ -1,11 +1,19 @@
 package com.example.trackhub
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class signpass : AppCompatActivity() {
+    private lateinit var b1: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signpass)
+        b1=findViewById(R.id.confirm1)
+        b1.setOnClickListener{
+            val intent= Intent(this,home::class.java)
+            startActivity(intent)
+        }
     }
 }
